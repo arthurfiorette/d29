d02779d0cb76d0313da6b078bc0acb3e2cca0509
Arthur Fiorette <github@arthur.place>
2022-06-12 13:16:10 +0000

    feat: qrcodes

--- images.sh
+++ images.sh
@@ -4,5 +4,5 @@
 
 for I in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
-  amzqr https://arthurfiorette.github.io/d29\#$I -p ./background.png -c -v 10 -d qrcodes -n qrcode-$I.png;
+  amzqr https://arthurfiorette.github.io/d29\#$I -p ./background-white.png -c -v 10 -d qrcodes -n qrcode-$I.png;
 done
