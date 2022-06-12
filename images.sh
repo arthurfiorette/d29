3e59e0466a8076e4ca92e44001bbc7d527bbeea2
Arthur Fiorette <github@arthur.place>
2022-06-12 13:09:54 +0000

    feat: added files

---
+++ images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# pip install amzqr
+
+for I in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+  amzqr https://arthurfiorette.github.io/d29\#$I -p ./background.png -c -v 10 -d qrcodes -n qrcode-$I.png;
+done
